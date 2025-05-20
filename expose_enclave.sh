52c7f82fbc8bda6c31314e108f6ea98109bc1772
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-05-20 19:16:32 +0530

    Initial commit

---
+++ expose_enclave.sh
@@ -0,0 +1,16 @@
+# Copyright (c), Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#!/bin/bash
+
+# Gets the encalve id and CID
+# expects there to be only one enclave running
+ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
+ENCLAVE_CID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveCID")
+
+sleep 5
+# Secrets-block
+SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id  --region us-east-1 | jq -r .SecretString)
+echo "$SECRET_VALUE" | jq -R '{"API_KEY": .}' > secrets.json
+
+cat secrets.json | socat - VSOCK-CONNECT:$ENCLAVE_CID:7777
+socat TCP4-LISTEN:3000,reuseaddr,fork VSOCK-CONNECT:$ENCLAVE_CID:3000 &
