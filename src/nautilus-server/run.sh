e5f0ea059403f6012a512793524694f0538414b4
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-06-13 18:03:53 +0530

    Update traffeic forwarder IP

--- src/nautilus-server/run.sh
+++ src/nautilus-server/run.sh
@@ -5,7 +5,7 @@
 # - Setup script for nautilus-server that acts as an init script
 # - Sets up Python and library paths
 # - Configures loopback network and /etc/hosts
-# - Waits for secrets.json to be passed from the parent instance. 
+# - Waits for secrets.json to be passed from the parent instance.
 # - Forwards VSOCK port 3000 to localhost:3000
 # - Optionally pulls secrets and sets in environmen variables.
 # - Launches nautilus-server
@@ -21,33 +21,8 @@ busybox ip addr add 127.0.0.1/32 dev lo
 busybox ip link set dev lo up
 
 # Add a hosts record, pointing target site calls to local loopback
-echo "127.0.0.1   localhost" > /etc/hosts
-echo "127.0.0.64   api.anthropic.com" >> /etc/hosts
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "127.0.0.1   localhost" >/etc/hosts
+echo "127.0.0.65   api.anthropic.com" >>/etc/hosts
 
 # == ATTENTION: code should be generated here that parses allowed_endpoints.yaml and populate domains here ===
 
@@ -58,7 +33,9 @@ JSON_RESPONSE=$(socat - VSOCK-LISTEN:7777,reuseaddr)
 # Sets all key value pairs as env variables that will be referred by the server
 # This is shown as a example below. For production usecases, it's best to set the
 # keys explicitly rather than dynamically.
-echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /tmp/kvpairs ; while IFS="=" read -r key value; do export "$key"="$value"; done < /tmp/kvpairs ; rm -f /tmp/kvpairs
+echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' >/tmp/kvpairs
+while IFS="=" read -r key value; do export "$key"="$value"; done </tmp/kvpairs
+rm -f /tmp/kvpairs
 
 # Run traffic forwarder in background and start the server
 # Forwards traffic from 127.0.0.x -> Port 443 at CID 3 Listening on port 800x
@@ -66,31 +43,7 @@ echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /tmp/kvpair
 
 # == ATTENTION: code should be generated here that added all hosts to forward traffic ===
 # Traffic-forwarder-block
-python3 /traffic_forwarder.py 127.0.0.64 443 3 8101 &
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+python3 /traffic_forwarder.py 127.0.0.65 443 3 8101 &
 
 # Listens on Local VSOCK Port 3000 and forwards to localhost 3000
 socat VSOCK-LISTEN:3000,reuseaddr,fork TCP:localhost:3000 &
