6e390a9a57c1674c2e669724a553885c9ec176fc
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-06-13 21:04:31 +0530

    Update enclave object IDs

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -2,7 +2,7 @@ export const DEFAULT_RPC_URL = 'https://api.cartridge.gg/x/starknet/sepolia'
 // export const RPC_NODE_URL = process.env.NEXT_PUBLIC_RPC_NODE_URL || DEFAULT_RPC_URL
 export const RPC_NODE_URL = DEFAULT_RPC_URL
 export const AGENT_REGISTRY_ADDRESS =
-  '0x02f6574e5b2e998e58dae9c256c8413d5bd4a9850a5f8162afe824b69cffeee7'
+  '0x6eea1a6fd3fbf870a043f311ad260c0dc6f384f3c869dd4ccfa550c2202bc485'
 
 export const INDEXER_BASE_URL = process.env.NEXT_PUBLIC_INDEXER_BASE_URL || 'http://localhost:4000'
 
@@ -23,15 +23,15 @@ export const TEXT_COPIES = {
 export const SUI_FAUCET_URL = 'https://faucet.sui.io/'
 
 export const TREASURY_ADDRESS = '0xe4e8af19c2943fc2e3cbefa2fca9149985fc3b3c3d9dd312902b2c26dc600d95'
-export const LOCAL_API_PATH = 'http://54.81.11.64:3000'
+export const LOCAL_API_PATH = 'http://3.92.163.15:3000'
 
 export const SUI_CONFIG = {
-  EXAMPLES_PACKAGE_ID: '0x057874a5368987ba134f388a9283887f88670f6730af3d01c9a3bb4d8b9bb6ad',
+  EXAMPLES_PACKAGE_ID: '0xe66533e7317be1f38ba0bdde5b58d000f6330cc06d31c4bbf3f22709eac13001',
   MODULE_NAME: 'sentinel',
   OTW_NAME: 'SENTINEL',
-  AGENT_REGISTRY: '0xebe58dadbc250211695411a09c4118e51cf83a8db1f418961298a87444f6a1fd',
-  ENCLAVE_OBJECT_ID: '0x5bc7b3dbe25f61535578e05b9c1979c197cf1962afff8d129de16483b1fd05d1',
-  AGENT_OBJECT_IDS: ['0xd6784e19fa6dd18f4a6f3c7c14e405f2208d223a18cc852bfbef6c7b3bf88ff5'],
+  AGENT_REGISTRY: '0x6eea1a6fd3fbf870a043f311ad260c0dc6f384f3c869dd4ccfa550c2202bc485',
+  ENCLAVE_OBJECT_ID: '0xb1eda22ce9f778835f1c14f513a3b849aad3a1f76074839de5cf4b2121f2804e',
+  AGENT_OBJECT_IDS: [''],
 }
 
 export const EXPLORER_BASE_URL = 'https://testnet.suivision.xyz'
